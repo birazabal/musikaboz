9c7c33001623cad265fe46a874d4b1f5518643ca
birazabal <birazabal@gmail.com>
2018-03-20 19:54:45 +0100

    botoiak frogatzen2

--- ikusi_erreprodukzio_zerrenda.php
+++ ikusi_erreprodukzio_zerrenda.php
@@ -1,8 +1,11 @@
 <html>
+<head></head><body>
+<input type='button' value='hurrengoa' id='hurrengoa'  />
+<input type='button' value='aurrekoa' id='aurrekoa'  />
 <!--http://devblog.lastrose.com/html5-audio-video-playlist/-->
 <audio id='audio' preload='auto' tabindex='0' controls='' >
   <div id="rz"></div>
-
+</body>
 <?php
 include 'zerbitzari_funtzioak.php';
 
@@ -17,7 +20,7 @@ $(document).ready(function () {
 		var playlist = $('#playlist');
 		var tracks = playlist.find('li a');
 		var len = tracks.length - 1;
-		audio[0].volume = .10;
+		audio[0].volume = .50;
 		audio[0].play();
 		playlist.on('click','a', function(e){
 			e.preventDefault();
@@ -35,6 +38,27 @@ $(document).ready(function () {
 			}
 			run($(link),audio[0]);
 		});
+		hurrengoa.addEventListener('click',function(e){
+			current++;
+			if(current == len){
+				current = 0;
+				link = playlist.find('a')[0];
+			}else{
+				link = playlist.find('a')[current];    
+			}
+			run($(link),audio[0]);		
+		});
+		aurrekoa.addEventListener('click',function(e){
+			current--;
+			if(current == -1){
+				current = len;
+				link = playlist.find('a')[len];
+			}else{
+				link = playlist.find('a')[current];    
+			}
+			run($(link),audio[0]);		
+		});
+		
 	}
 	function run(link, player){
 			player.src = link.attr('href');
