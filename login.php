85258c665ba62d69933ce3b390553592fd0f1c4d
birazabal <birazabal@gmail.com>
2018-04-22 10:02:27 +0200

    logina inplementatzen

--- login.php
+++ login.php
@@ -3,37 +3,11 @@ include 'zerbitzari_funtzioak.php';
 //LOGIN PETRAL BAT
 //Jquery AJAX bidez datozen balioak jaso, izena eta pass 
 //datu balioak hobeto kontrolatu
-if ((isset($_POST["izena"])) && (isset($_POST["pass"]))){
-   if ($_POST["izena"] != "" && $_POST["pass"] != ""){	
-	//$mezua = "ADI: Izena eta pass jasota </br>";	
-	$izena = $_POST["izena"];
-	$pass = $_POST["pass"];
-	//funtzioa erabili
-	$emaitza = izena_eman($izena,$pass);
-	if ($emaitza == "wtf"){
-		$mezua = "ERABILTZAILE IZENA edo PASAHITZA gaizki daude";		
-	}else{
-		//DENA ONDO BERAZ-> sesioa hasi + ondo mezua bidali berbidalketarako js-n
-		//JS-n berbidalketa ez da segurua, kontrolatu 
-		session_start();
-		$_SESSION["izena"] = $izena;
-		$mezua = "ondo";
-		//header("Location:http://192.168.0.158/musikaboz/musikaboz bezero/");
-		
-	} 
-   	
-   }else{
-	$mezua = "ADI: Ezin dira gelaxkak hutsik utzi!";
-   }	
-	//$mezua = "kaixo" . $izena . " " .$pass . ">>" . "<a target='_blank' href='http://192.168.0.158/musikaboz/musikaboz bezero/'>link</a>";
-}else{
-	$mezua = "ADI: Datuak ONDO bete mesedez !!";
-}
 
 
-echo $mezua;
-//oraingoz inprimatu badabilela frogatzeko
-
+$ktr = new erabiltzaileKontrola();
+$ktr->login_kontrola();
+//login_kontrola();
 
 
 
--- zerbitzari_funtzioak.php
+++ zerbitzari_funtzioak.php
@@ -24,6 +24,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
 }
 cors_hasieratu();
+
+class erabiltzaileKontrola{
+	function login_kontrola(){
+		try{
+			if ((isset($_POST["izena"])) && (isset($_POST["pass"]))){
+   				if ($_POST["izena"] != "" && $_POST["pass"] != ""){	
+					//$mezua = "ADI: Izena eta pass jasota </br>";	
+					$izena = $_POST["izena"];
+					$pass = $_POST["pass"];
+					$db = new Datubasea();
+					//funtzioa erabili
+					$emaitza = $db->begiratu_erab($izena,$pass);
+					if ($emaitza == "wtf"){
+						$mezua = "ERABILTZAILE IZENA edo PASAHITZA gaizki daude" . $emaitza;		
+					}else{
+						//DENA ONDO BERAZ-> sesioa hasi + ondo mezua bidali berbidalketarako js-n
+						//JS-n berbidalketa ez da segurua, kontrolatu 
+						session_start();
+						$_SESSION["izena"] = $izena;
+						$mezua = "ondo";
+						//header("Location:http://192.168.0.157/musikaboz/musikaboz bezero/");
+			
+					} 
+   		
+   				}else{
+					$mezua = "ADI: Ezin dira gelaxkak hutsik utzi!";
+   				}	
+				//$mezua = "kaixo" . $izena . " " .$pass . ">>" . "<a target='_blank' href='http://192.168.0.158/musikaboz/musikaboz bezero/'>link</a>";
+			}else{
+				$mezua = "ADI: Datuak ONDO bete mesedez !!";
+			}
+		}
+		catch(Exception $e) {
+  				printf("[!] Mezua: %s\n", $e->getMessage());
+		}
+		echo $mezua;
+	}
+
+
+
+}
 class Datubasea{
 	
 	//URL nabigatzaile bitartez abestiak non eta DIR fitxategi sisteman abestiak non
@@ -184,7 +225,7 @@ class Datubasea{
 				$lerroak = $sententzia->num_rows;
 				//lerrorik bada, existitzen da
 				if ($lerroak > 0) {
-					return $erantzuna;			
+					return "ondo";			
 				}else{
 					return "wtf";
 				}
